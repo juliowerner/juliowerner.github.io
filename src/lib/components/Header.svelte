c517d8373ea8a888aba2134d77f5d5dc5fac6e36
Julio Werner <juliowernery@gmail.com>
2024-10-10 21:57:16 -0300

    add button to nav fix colors

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,14 +1,15 @@
 <script>
 	import DarkModeButton from './DarkModeButton.svelte';
+	import Button from './Button.svelte';
 </script>
 
-<header class="flex flex-col z-20 sticky top-0 bg-base-100 border-b border-gray-500">
+<header class="flex flex-col z-20 sticky top-0 bg-base-100 border-b border-base-300 shadow-md">
 	<div class="max-w-[1400px] mx-auto w-full flex items-center justify-around p-4 py-6">
 		<a href="/">
 			<h2 class="text-2xl font-bold">Julio Werner</h2>
 		</a>
 		<nav class="flex text-base items-center space-x-4">
-			<a href="/" class="px-3 py-1.5 rounded-full text-sm hover:bg-base-200">About</a>
+			<Button name="Home" href="/" />
 		</nav>
 		<div class="max-sm:hidden">
 			<DarkModeButton />
