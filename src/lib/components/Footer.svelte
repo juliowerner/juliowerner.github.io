1adc7c37b850a3c0d3f50b87586c926a2ac9cb32
Julio Werner <juliowernery@gmail.com>
2024-10-10 22:34:06 -0300

    add social media to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,9 +1,12 @@
 <script>
+	import SocialMedia from './SocialMedia.svelte';
+
 	let year = new Date().getFullYear();
 </script>
 
 <footer class="border-t border-base-300 mt-8">
-	<div class="py-5 text-center">
+	<SocialMedia />
+	<div class="my-2 text-center">
 		<span class="text-md">Julio Werner's Blog © {year}</span>
 	</div>
 </footer>
