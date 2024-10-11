d1b91559ff8c8c631b5872df285e48542aabf851
Julio Werner <juliowernery@gmail.com>
2024-10-10 21:56:49 -0300

    set footter color and margin

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,7 +2,7 @@
 	let year = new Date().getFullYear();
 </script>
 
-<footer class="border-t border-neutral-800">
+<footer class="border-t border-base-300 mt-8">
 	<div class="py-5 text-center">
 		<span class="text-md">Julio Werner's Blog © {year}</span>
 	</div>
