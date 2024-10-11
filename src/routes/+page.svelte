f90d71c23818e0262b955a18e7a917d4eee54316
Julio Werner <juliowernery@gmail.com>
2024-10-10 22:09:48 -0300

    Intro text rewrited

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,23 +3,21 @@
 </script>
 
 <SectionWrapper>
-	<span class="flex flex-row">
-		<h1 class="text-3xl font-bold px-2">Hey there!</h1>
-		<img class="h-8 w-8" src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" />
+	<span class="flex flex-row items-center my-2">
+		<h1 class="text-3xl font-bold mr-2">Hey there!</h1>
+		<img class="h-8 w-8" alt="" src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" />
 	</span>
 	<article class="prose xl:prose-lx dark:prose-invert">
 		<p>
-			My name is Julio Werner and I'm a PhD in Water Resources and Environmental Engineering and
-			Software Developer.
+			My name is Julio Werner. I am a PhD environmental engineer and software developer. Feel free
+			to connect with me on social media to discuss:
 		</p>
-
-		<p>Reach me out on social media to discuss:</p>
 		<ul>
 			<li>Water Resources and Environmental Engineering</li>
 			<li>Software Development</li>
 			<li>Machine Learning</li>
 			<li>Web Development</li>
-			<li>Anything else you want to talk about</li>
 		</ul>
+		<p>Or anything else you want to talk about.</p>
 	</article>
 </SectionWrapper>
